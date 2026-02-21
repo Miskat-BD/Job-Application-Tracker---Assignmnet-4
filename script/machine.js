06402500d54dfb17e54b3b8737be8a9dcb5d7ec0
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-02-22 04:31:55 +0600

    3rd commit

---
+++ script/machine.js
@@ -0,0 +1,5 @@
+function calculateCount() {
+    total.innerText = allCards.children.length;
+    interviewCount.innerHTML = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
--- script/script.js
+++ script/script.js
@@ -1,2 +1,16 @@
-const p = document.getElementById('p');
-p.style.color ='red'
+let total = document.getElementById('total-jobs');
+let interviewCount = document.getElementById('total-interview');
+let rejectedCount = document.getElementById('total-rejected');
+let allCards = document.getElementById("all-cards");
+let buttons = document.getElementById('buttons');
+let mainContainer = document.querySelector('main');
+
+let interviewList = [];
+let rejectedList = [];
+
+// Calculate the counters
+calculateCount();
+
+function toggleStyle(id){
+    console.log("clicked",)
+}
